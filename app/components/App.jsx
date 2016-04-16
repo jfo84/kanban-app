2f6c9cea877fc5148d5f494cd76a979d9bb3c7b9
Jack Ford <jford@salsify.com>
2016-04-16 11:23:09 -0500

    Added alt. Soon to be replaced by immutable/redux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,41 @@
-import uuid from 'node-uuid';
+// import uuid from 'node-uuid';
 import React from 'react';
-import Notes from './Notes.jsx'
+import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'LEARN REACT'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do dishes'
-        }
-      ]
-    };
+    // this.state = {
+    //   notes: [
+    //     {
+    //       id: uuid.v4(),
+    //       task: 'Learn Webpack'
+    //     },
+    //     {
+    //       id: uuid.v4(),
+    //       task: 'LEARN REACT'
+    //     },
+    //     {
+    //       id: uuid.v4(),
+    //       task: 'Do dishes'
+    //     }
+    //   ]
+    // };
+    this.state = NoteStore.getState();
+  }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    this.setState(state);
   }
 
   render() {
@@ -36,36 +50,51 @@ export default class App extends React.Component {
     );
   }
 
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'NEEEEEEWWWWWWWMMMMMMAAAAAANNNNN'
-      }])
-    })
+  // addNote = () => {
+  //   this.setState({
+  //     notes: this.state.notes.concat([{
+  //       id: uuid.v4(),
+  //       task: 'NEEEEEEWWWWWWWMMMMMMAAAAAANNNNN'
+  //     }])
+  //   })
+  // }
+  addNote() {
+    NoteActions.create({task: 'New Task'});
   }
 
-  editNote = (id, task) => {
+  // editNote = (id, task) => {
+  //   if(!task.trim()) {
+  //     return;
+  //   }
+  //
+  //   const notes = this.state.notes.map(note => {
+  //     if(note.id === id && task) {
+  //       note.task = task;
+  //     }
+  //
+  //     return note;
+  //   })
+  //
+  //   this.setState({notes});
+  // }
+  editNote(id, task) {
+    // Don't modify if trying to set an empty value
     if(!task.trim()) {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-
-      return note;
-    })
-
-    this.setState({notes});
+    NoteActions.update({id, task});
   }
 
-  deleteNote = (id, e) => {
+  // deleteNote = (id, e) => {
+  //   e.stopPropagation();
+  //
+  //   this.setState({
+  //     notes: this.state.notes.filter(note => note.id !== id)
+  //   });
+  // };
+  deleteNote(id, e) {
     e.stopPropagation();
-
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
+    NoteActions.delete(id);
+  }
 }
